857f854a39b2e6f888174af210a0ed4fcbea97af
kaline <meqsquitabfkaline@gmail.com>
2020-02-13 18:47:29 -0300

    add code

--- KyteBank/ContaCorrente.cs
+++ KyteBank/ContaCorrente.cs
@@ -14,7 +14,33 @@ namespace KyteBank
             public int agencia;
             public int numero;
             public double saldo;
+            public int limiteConta;
+            public bool Transferir(double valor)
+            {
+                 if(this.limiteConta < valor)
+                 {
+                     return false;
+                 }
+                else
+                {
+                this.saldo += valor;
+                    return true;
+                }
+            }
 
-        
+            public bool Sacar(double valor)
+            {
+
+            if (this.saldo < valor)
+            {
+                return false;
+            }
+            else
+            {
+                this.saldo -= valor;
+                return true;
+            }
+                
+            }
     }
 }
