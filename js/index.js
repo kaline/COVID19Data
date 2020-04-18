d072544d1a85bf61e257018b67f4c17689fcb68e
kaline <mesquitabfkaline@gmail.com>
2020-04-18 18:29:56 -0300

    :poop: add data from other countries

--- js/index.js
+++ js/index.js
@@ -4,48 +4,72 @@ url = "https://pomber.github.io/covid19/timeseries.json";
         return response.json();
     })
     .then((data) => {
-        //document.getElementById("data").innerText =  JSON.stringify(data);
-        //document.getElementById("dataBrazil").innerText = JSON.stringify(data["Brazil"]);
-        var obj = JSON.parse(JSON.stringify(data));
-
-        var country = "";
+        
+        var obj = data;
+        var countries = "";
         var selectPairs = Object.entries(obj);
         console.log(selectPairs);
+
         for(var k = 0; k < selectPairs.length; k++){
-            country += ' <option value="' + selectPairs[k][0] + '">' + selectPairs[k][0] +'</option>';
+            countries += ' <option id="teste" value="' + selectPairs[k][0] + '">' + selectPairs[k][0] +'</option>';     
+           
+        }
+
+        function onMousedownHandler(e){
+            var el = e.currentTarget;
+ 
+            if(el.hasAttribute('size') && el.getAttribute('size') == '1'){
+                e.preventDefault();
+            }
+ 
+         }
+        function onClickHandler(e){
+            var el = e.currentTarget;
+ 
+            if(el.hasAttribute('size') == '1'){
+                el.className += "selectOpen";
+                el.setAttribute('size', '5');
+            }else{
+                el.className = '';
+                el.setAttribute('size', '1');
+            }
+ 
+        }
+        document.getElementById("dropdown-content").innerHTML = countries;
+        document.getElementById('dropdown-content').addEventListener('click', onClickHandler);
+        document.getElementById('dropdown-content').addEventListener('mousedown', onMousedownHandler);
+ 
+
+        window.getCountry = function() {
+            var country = document.getElementById("dropdown-content");
+            var index = country.selectedIndex;
+            console.log(index)
+            console.log(country)
+            var displayCountry = country.value;
+            var objCountry = JSON.parse(JSON.stringify(country))
+            document.getElementById("dataBrazil").innerHTML = displayCountry;
+
+            console.log(objCountry)
+            const countryDates = data[displayCountry];
+            const lastDate = countryDates.length - 1;
+            document.getElementById("dateTodayConfirmed").innerHTML = countryDates[lastDate].confirmed;
+            document.getElementById("dateTodayRecovered").innerHTML = countryDates[lastDate].recovered;
+            document.getElementById("dateTodayDead").innerHTML = countryDates[lastDate].deaths;
+
+            for(var i = 0; i < lastDate; i++){
+                var todayCases = countryDates[lastDate].confirmed - countryDates[i].confirmed;
+                document.getElementById("dateTodayCases").innerHTML = todayCases;
+            }
+
+            var mortality = ((countryDates[lastDate].deaths/countryDates[lastDate].confirmed)*100).toFixed(2);
+            document.getElementById("dateTodayMortality").innerHTML = mortality + "%";
+            var activeCases = (countryDates[lastDate].confirmed-(countryDates[lastDate].deaths + countryDates[lastDate].recovered));
+            document.getElementById("dateTodayActive").innerHTML = activeCases;
             
         }
-        document.getElementById("dropdown-content").innerHTML = country;
-        
-
-       document.getElementById('dropdown-content').addEventListener('click', onClickHandler);
-       document.getElementById('dropdown-content').addEventListener('mousedown', onMousedownHandler);
-
-       function onMousedownHandler(e){
-           var el = e.currentTarget;
-
-           if(el.hasAttribute('size') && el.getAttribute('size') == '1'){
-               e.preventDefault();
-           }
-
-       }
-       function onClickHandler(e){
-           var el = e.currentTarget;
-
-           if(el.hasAttribute('size') == '1'){
-               el.className += "selectOpen";
-               el.setAttribute('size', '5');
-           }else{
-               el.className = '';
-               el.setAttribute('size', '1');
-           }
-
-       }
-
-
-
+      
         var objLength = obj.Brazil.length;
-        console.log(obj.Brazil.length);
+        
         var i;
         for(i= 0; i < objLength; i++){
             var j;
